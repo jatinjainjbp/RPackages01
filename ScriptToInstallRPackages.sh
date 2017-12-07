976266c4b2718ed175711813bf7c38aa1b612927
jatinjainjbp <34292717+jatinjainjbp@users.noreply.github.com>
2017-12-07 10:04:00 +0530

    Update ScriptToInstallRPackages.sh

--- ScriptToInstallRPackages.sh
+++ ScriptToInstallRPackages.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/jatinjainjbp/RPackages01/master/InstallRPackages.R
+wget "https://raw.githubusercontent.com/jatinjainjbp/RPackages01/master/InstallRPackages.R"
 R CMD BATCH InstallRPackages.R
